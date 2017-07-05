da866c2204877fa404203ff8f8675431dc34bb10
Zachary Wassall <z.wassall@gmail.com>
2017-07-05 17:28:37 -0400

    URI encode mongo auth

--- test/app.js
+++ test/app.js
@@ -6,17 +6,24 @@ app.post("/api/test", createMessage);
 app.delete("/api/test/:id", deleteMessage);
 
 var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+
+if(process.env.MLAB_USERNAME) {
+    connectionString = encodeURIComponent(process.env.MLAB_USERNAME) + ":" +
+        encodeURIComponent(process.env.MLAB_PASSWORD) + "@" +
+        process.env.MLAB_HOST + ':' +
+        process.env.MLAB_PORT + '/' +
+        process.env.MLAB_APP_NAME;
 }
-// Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
-// above with your own URL given to you by mLab
 
 var mongoose = require("mongoose");
-mongoose.connect(connectionString);
+// https://stackoverflow.com/a/23344660
+mongoose.connect(connectionString, {
+        uri_decode_auth: true
+    }, function(err, db) {
+
+    }
+);
+
 mongoose.Promise = q.Promise;
 
 var TestSchema = mongoose.Schema({
