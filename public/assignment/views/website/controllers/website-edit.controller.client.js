0c2c31a0fd5f7bc3ebfbade916c336f97d516b15
Zachary Wassall <z.wassall@gmail.com>
2017-07-25 17:32:02 -0400

    Implement edit website view and controller

--- public/assignment/views/website/controllers/website-edit.controller.client.js
+++ public/assignment/views/website/controllers/website-edit.controller.client.js
@@ -3,7 +3,32 @@
         .module("WebAppMaker")
         .controller("editWebsiteController", editWebsiteController);
 
-        function editWebsiteController() {
+        function editWebsiteController($routeParams, $location, websiteService) {
             var model = this;
+
+            model.validate = validate;
+            model.updateWebsite = updateWebsite;
+            model.deleteWebsite = deleteWebsite;
+
+            model.uid = $routeParams["uid"];
+            model.websites = websiteService.findWebsitesByUser(model.uid);
+            model.wid = $routeParams["wid"];
+            model.website = websiteService.findWebsiteById(model.wid);
+            var websiteStr = JSON.stringify(model.website);
+
+            function validate() {
+                return !!model.website.name
+                    && websiteStr !== JSON.stringify(model.website);
+            }
+
+            function updateWebsite() {
+                websiteService.updateWebsite(model.wid, model.website);
+                $location.url("user/" + model.uid + "/website");
+            }
+
+            function deleteWebsite() {
+                websiteService.deleteWebsite(model.wid);
+                $location.url("user/" + model.uid + "/website");
+            }
         }
 })();
