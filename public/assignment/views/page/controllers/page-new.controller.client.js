966da2a3140dc597699e402a90015dee212c0107
Zachary Wassall <z.wassall@gmail.com>
2017-07-25 17:59:28 -0400

    Implement new page view and controller

--- public/assignment/views/page/controllers/page-new.controller.client.js
+++ public/assignment/views/page/controllers/page-new.controller.client.js
@@ -3,7 +3,25 @@
         .module("WebAppMaker")
         .controller("newPageController", newPageController);
 
-        function newPageController() {
+        function newPageController($routeParams, $location, pageService) {
             var model = this;
+
+            model.validate = validate;
+            model.createPage = createPage;
+
+            model.uid = $routeParams["uid"];
+            model.wid = $routeParams["wid"];
+            model.pages = pageService.findPagesByWebsiteId(model.wid);
+            model.page = {};
+            model.page.websiteId = model.wid;
+
+            function validate() {
+                return !!model.page.name;
+            }
+
+            function createPage() {
+                pageService.createPage(model.page);
+                $location.url("user/" + model.uid + "/website/" + model.wid + "/page");
+            }
         }
 })();
