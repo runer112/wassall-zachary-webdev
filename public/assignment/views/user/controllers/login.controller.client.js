276f643855cc3141247f9c3364a6f894166ce169
Zachary Wassall <z.wassall@gmail.com>
2017-07-25 00:17:27 -0400

    Implement login view and controller

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -3,11 +3,22 @@
         .module("WebAppMaker")
         .controller("loginController", loginController);
 
-        function loginController() {
+        function loginController($location, userService) {
             var model = this;
+
+            model.login = login;
 
             function init() {
             }
             init();
+
+            function login(user) {
+                user = userService.findUserByCredentials(user.username, user.password);
+                if (user) {
+                    $location.url("user/" + user._id);
+                } else {
+                    model.errorMessage = "Invalid username or password";
+                }
+            }
         }
 })();
