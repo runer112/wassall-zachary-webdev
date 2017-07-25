4a01d32a2c3fca0f287e1f3d1d7d4bd5bc61dcdc
Zachary Wassall <z.wassall@gmail.com>
2017-07-25 16:27:15 -0400

    Implement new website view and controller

--- public/assignment/views/website/controllers/website-new.controller.client.js
+++ public/assignment/views/website/controllers/website-new.controller.client.js
@@ -3,7 +3,19 @@
         .module("WebAppMaker")
         .controller("newWebsiteController", newWebsiteController);
 
-        function newWebsiteController() {
+        function newWebsiteController($routeParams, $location, websiteService) {
             var model = this;
+
+            model.createWebsite = createWebsite;
+
+            model.uid = $routeParams["uid"];
+            model.websites = websiteService.findWebsitesByUser(model.uid);
+            model.website = {};
+            model.website.developerId = model.uid;
+
+            function createWebsite(website) {
+                websiteService.createWebsite(website);
+                $location.url("user/" + model.uid + "/website");
+            }
         }
 })();
