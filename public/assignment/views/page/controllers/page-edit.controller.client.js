7b022bee0ca5348eda311a091629517149b68c0e
Zachary Wassall <z.wassall@gmail.com>
2017-07-25 18:05:12 -0400

    Implement edit page view and controller

--- public/assignment/views/page/controllers/page-edit.controller.client.js
+++ public/assignment/views/page/controllers/page-edit.controller.client.js
@@ -3,7 +3,32 @@
         .module("WebAppMaker")
         .controller("editPageController", editPageController);
 
-        function editPageController() {
+        function editPageController($routeParams, $location, pageService) {
             var model = this;
+
+            model.validate = validate;
+            model.updatePage = updatePage;
+            model.deletePage = deletePage;
+
+            model.uid = $routeParams["uid"];
+            model.wid = $routeParams["wid"];
+            model.pid = $routeParams["pid"];
+            model.page = pageService.findPageById(model.pid);
+            var pageStr = JSON.stringify(model.page);
+
+            function validate() {
+                return !!model.page.name
+                    && pageStr !== JSON.stringify(model.page);
+            }
+
+            function updatePage() {
+                pageService.updatePage(model.pid, model.page);
+                $location.url("user/" + model.uid + "/website/" + model.wid + "/page");
+            }
+
+            function deletePage() {
+                pageService.deletePage(model.pid);
+                $location.url("user/" + model.uid + "/website/" + model.wid + "/page");
+            }
         }
 })();
