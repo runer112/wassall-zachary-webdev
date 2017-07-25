8f5d5391637fa2ace36a3a9fc70d4d36aade7f7f
Zachary Wassall <z.wassall@gmail.com>
2017-07-25 00:17:12 -0400

    Implement user service

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -1,3 +1,70 @@
-/**
- * Created by zwass on 7/24/2017.
- */
+(function() {
+    angular
+        .module("WebAppMaker")
+        .factory("userService", userService);
+
+    function userService() {
+
+        var users = [
+            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" },
+        ];
+
+        var nextId = 1000;
+
+        var api = {
+            "createUser"            : createUser,
+            "findUserById"          : findUserById,
+            "findUserByUsername"    : findUserByUsername,
+            "findUserByCredentials" : findUserByCredentials,
+            "updateUser"            : updateUser,
+            "deleteUser"            : deleteUser,
+        };
+
+        return api;
+
+        function createUser(user) {
+            user._id  = nextId++;
+            users.push(user);
+            return user;
+        }
+
+        function findUserById(userId) {
+            return users.find(function (user) {
+                return user._id === userId;
+            });
+        }
+
+        function findUserByUsername(username) {
+            return users.find(function (user) {
+                return user.username === username;
+            });
+        }
+
+        function findUserByCredentials(username, password) {
+            return users.find(function (user) {
+                return user.username === username && user.password === password;
+            });
+        }
+
+        function updateUser(userId, user) {
+            var index = findIndexOfUserById(userId);
+            users[index] = user;
+            return user;
+        }
+
+        function deleteUser(userId, user) {
+            var index = findIndexOfUserById(userId);
+            users.splice(index, 1);
+        }
+
+        function findIndexOfUserById(userId) {
+            return users.findIndex(function (user) {
+                return user._id === userId;
+            });
+        }
+
+    }
+})();
