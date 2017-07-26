d1cf89b64eb07048be0485af2985c3ec31bf96f3
Zachary Wassall <z.wassall@gmail.com>
2017-07-25 21:34:36 -0400

    Delete unitialized widgets upon loading the widget list

--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -14,7 +14,18 @@
         model.uid = $routeParams["uid"];
         model.wid = $routeParams["wid"];
         model.pid = $routeParams["pid"];
-        model.widgets = widgetService.findWidgetsByPageId(model.pid);
+        var widgets = widgetService.findWidgetsByPageId(model.pid);
+
+        // delete unintialized widgets
+        model.widgets = [];
+        widgets.forEach(function (widget) {
+            // _id, pageId, and widgetType should always be present, so check for more than 3 keys
+            if (Object.keys(widget).length > 3) {
+                model.widgets.push(widget);
+            } else {
+                widgetService.deleteWidget(widget._id);
+            }
+        });
 
         function getWidgetUrl(widgetType) {
             return "views/widget/templates/widget-" + widgetType + ".view.client.html";
